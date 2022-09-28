2ac82a38fd11ff0d39750819265aa6b972d75945
Maria Sol Gosende <msp.01209@gmail.com>
2022-09-28 12:20:18 -0300

    4_boton_createursdemode

---
+++ 4_boton_createursdemode/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Buttton from './components/Button/Buttton';
+
+function App() {
+  return (
+    <div className="App">
+     <Buttton/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 4_boton_createursdemode/src/components/Button/Buttton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Buttton = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button> <a href="https://www.viktor-rolf.com/"> Vikktor & Rolf </a></button>
+        <button> <a href="https://www.viviennewestwood.com/en/"> Viviene Westwood </a></button>
+        <button> <a href="https://www.ysl.com/en-en"> Yves Saint Laurent </a></button>
+      </header>
+    </div>
+  )
+}
+
+export default Buttton
